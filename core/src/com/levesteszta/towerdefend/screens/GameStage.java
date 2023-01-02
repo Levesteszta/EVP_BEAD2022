fb168bf44352842617f3e209c97e6e7b8ee15fa6
NvG8956XD02 <gutenburg429@gmail.com>
2023-01-03 00:22:56 +0100

    "Kész" : pause már értelmesen néz ki

--- core/src/com/levesteszta/towerdefend/screens/GameStage.java
+++ core/src/com/levesteszta/towerdefend/screens/GameStage.java
@@ -5,15 +5,13 @@ import static com.levesteszta.towerdefend.helpers.Artist.*;
 import com.levesteszta.towerdefend.MapGen.TileGrid;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -40,7 +38,7 @@ public class GameStage extends ScreenAdapter{
         this.assetManager = assetManager;
         this.skin = assetManager.get(SKIN);
         room = new TileGrid(TILE_SIZE*2, TILE_SIZE*6, WINDOW_WIDTH-(TILE_SIZE*2), WINDOW_HEIGHT-(TILE_SIZE*1));
-        this.pause = new Window("PAUSE", skin);
+        pause = new Window("", skin, "mine");
         Clock.Play();
     }
 
@@ -49,6 +47,37 @@ public class GameStage extends ScreenAdapter{
         Clock.Stop();
         myGame.dispose();
         GameStage.this.game.setScreen(new GameOverScreen(game, assetManager));
+    }
+    
+    public void setMenuUp(){
+        TextButton returnButton = new TextButton("Return", skin);
+        returnButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Clock.Play();
+                pause.setVisible(false);
+            }
+        });
+        TextButton exitButton = new TextButton("Exit", skin);
+        exitButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("Exit...");
+                gameTable.clear();
+                GameStage.this.game.dispose();
+                Gdx.app.exit();
+            }
+        });
+        pause.getTitleTable().padTop(50).add(new Label("Pause", skin)).height(50).expand().fillX();
+        pause.add(returnButton).width(300).height(45).center().padBottom(10).row();
+        pause.add(exitButton).width(300).height(45).center().row();
+        pause.setSize(WINDOW_WIDTH/3, WINDOW_HEIGHT/3);
+        pause.setPosition(WINDOW_WIDTH/2-pause.getWidth()/2,WINDOW_HEIGHT/2-pause.getHeight()/2);
+        pause.setMovable(false);
+        pause.setResizable(false);
+        pause.setVisible(false);
+
+        TowerDefend.stage.addActor(pause);
     }
 
     
@@ -62,24 +91,8 @@ public class GameStage extends ScreenAdapter{
             gameTable = new Table();
             gameTable.setFillParent(true);
 
-            TextButton returnButton = new TextButton("Return", skin);
-            returnButton.addListener(new ClickListener(){
-                @Override
-                public void clicked(InputEvent event, float x, float y) {
-                    Clock.Play();
-                    pause.setVisible(false);
-                }
-            });
-            pause.padTop(64);
-            pause.add(returnButton).row();
-            pause.setSize(WINDOW_WIDTH/2, WINDOW_HEIGHT/2);
-            pause.setPosition(WINDOW_WIDTH/2-pause.getWidth()/2,WINDOW_HEIGHT/2-pause.getHeight()/2);
-            pause.setMovable(false);
-            pause.setResizable(false);
-            pause.setVisible(false);
-
             TowerDefend.stage.addActor(gameTable);
-            TowerDefend.stage.addActor(pause);
+            setMenuUp();
 
             myGame = new myGame(room);
             TowerDefend.batch.setProjectionMatrix(camera.combined);
